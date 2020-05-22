b671464e1c7b76e153f279f1546fa3d2c3757936
Ruidy Nemausat <ruidy.nemausat@gmail.com>
2020-05-22 13:33:18 +0200

    improve: add loadProduct action to product store

--- src/store/product/index.ts
+++ src/store/product/index.ts
@@ -1,5 +1,5 @@
-import {createSlice} from '@reduxjs/toolkit';
-import IProduct from '../../models/IProduct';
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+import IProduct, {mockProducts} from '../../models/IProduct';
 import {RootState} from '..';
 
 interface ISliceState {
@@ -7,19 +7,31 @@ interface ISliceState {
 }
 
 const initialState: ISliceState = {
-  products: [],
+  products: mockProducts,
 };
 /** product state slice */
 const productSlice = createSlice({
   name: 'product',
   initialState,
-  reducers: {},
+  reducers: {
+    /**
+     * store the received products in the store
+     * @param products IProduct[]
+     */
+    loadProducts: (state, action: PayloadAction<IProduct[]>) => ({
+      ...state,
+      products: action.payload,
+    }),
+  },
 });
 
 /** extract product list from the store */
 export const selectProducts = (state: RootState) => ({
   products: state.product.products,
 });
+
+// export actions
+export const {loadProducts} = productSlice.actions;
 
 /** product reducer */
 export default productSlice.reducer;
