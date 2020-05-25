38e71e97e9f03276472f4cf175a67811669c4793
Ruidy Nemausat <ruidy.nemausat@gmail.com>
2020-05-25 16:25:46 +0200

    improve: update IProduct fields

--- src/models/IProduct.ts
+++ src/models/IProduct.ts
@@ -4,10 +4,22 @@
 export default interface IProduct {
   id: string;
   name: string;
+  picture: string;
+  description: string;
 }
 
 // sample names to generate mocks
 const names = ['boy', 'girl', 'neutral'];
+const pics = [
+  'https://source.unsplash.com/weekly?boy',
+  'https://source.unsplash.com/weekly?girl',
+  'https://source.unsplash.com/weekly?neutral',
+];
+const description = [
+  'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo, consectetur?',
+  'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur, animi fugiat repellat expedita quasi iure.',
+  'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur, animi fugiat repellat expedita quasi iure.',
+];
 
 /**
  * mock product list to be used in dev and tests
@@ -15,4 +27,6 @@ const names = ['boy', 'girl', 'neutral'];
 export const mockProducts: IProduct[] = names.map((name, i) => ({
   id: i.toString(),
   name,
+  picture: pics[i],
+  description: description[i],
 }));
