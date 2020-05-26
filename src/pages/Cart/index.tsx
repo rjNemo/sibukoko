d7cb93a88ab4a0ee5f9f86df8f598d008b8c8a4e
Ruidy Nemausat <ruidy.nemausat@gmail.com>
2020-05-26 13:09:33 +0200

    feat(cart): add cartpage file

---
+++ src/pages/Cart/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Container, Typography} from '@material-ui/core';
+
+const CartPage = () => (
+  <Container>
+    <Typography>Cart</Typography>
+  </Container>
+);
+
+export default CartPage;
